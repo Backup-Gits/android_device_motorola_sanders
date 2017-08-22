8a50c67cb3f53e978af4f023059aa014662fdb8b
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:17:09 +0200

    sanders: build Vibrator HIDL HAL interface

--- device.mk
+++ device.mk
@@ -158,6 +158,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     libwpa_client \
