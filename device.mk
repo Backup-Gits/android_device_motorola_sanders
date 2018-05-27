ebf89b1236dcb59bba54ebda5fe5d273e4cca8e4
Michael Bestas <mkbestas@lineageos.org>
2018-05-27 05:18:54 +0300

    sanders: sensors: convert to sanders target

--- device.mk
+++ device.mk
@@ -198,7 +198,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-	android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service.sanders
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/vendor/etc/sensors/hals.conf
