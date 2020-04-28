57f3130f49c9aca4860319826d8829e2e20a23b5
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2020-04-28 23:53:01 +0530

    sanders: build Snap

--- device.mk
+++ device.mk
@@ -46,6 +46,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
     
+PRODUCT_PACKAGES += \
+    Snap
+    
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
