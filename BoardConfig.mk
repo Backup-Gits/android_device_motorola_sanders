8fef7ab1bf0fbe495cb2ccf02e371ff4583b7554
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2020-04-28 11:49:00 +0530

    sanders: define common WiFi board flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,5 +72,18 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Use Snapdragon LLVM if available on build server
 TARGET_USE_SDCLANG := true
 
+# Wifi
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_HAS_QCOM_WLAN_SDK          := true
+BOARD_WLAN_DEVICE                := qcwcn
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_P2P          := "p2p"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+
 # Device Tree
 BOARD_USES_DT := true
