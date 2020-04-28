7d9f2d990fbcc65e045832ef4e83000d63d6d1f1
Paul Keith <javelinanddart@gmail.com>
2020-04-28 22:16:36 +0530

    sanders: build LiveDisplay sysfs service
    - Add supported interfaces to manifest while we are at it

--- device.mk
+++ device.mk
@@ -50,7 +50,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay@1.0-service-sysfs
 
 # Media
 PRODUCT_COPY_FILES += \
