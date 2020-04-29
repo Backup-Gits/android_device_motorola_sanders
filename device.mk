8c24638c7af99b99eb772220068413a306c24825
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2020-04-30 00:09:43 +0530

    sanders: build Thermal HIDL HAL interfaces
    - import thermal config from 8-OPS28.65-36-14-63857-release-keys

--- device.mk
+++ device.mk
@@ -203,6 +203,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/vendor/etc/sensors/hals.conf
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+
 # Trust
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
