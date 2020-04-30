7c2f4eeee574821e6a7afe3c8047ffa8e48305af
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2020-04-30 17:59:47 +0530

    sanders: implement LineageActions

--- device.mk
+++ device.mk
@@ -151,6 +151,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# LineageActions
+PRODUCT_PACKAGES += \
+    LineageActions
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
