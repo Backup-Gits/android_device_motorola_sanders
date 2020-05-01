f74303ec788c8716967aa62159b3be016935f7fa
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2020-05-01 12:31:01 +0530

    sanders: inherit 64-bit configs

--- lineage_sanders.mk
+++ lineage_sanders.mk
@@ -13,6 +13,9 @@
 # limitations under the License.
 #
 
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
